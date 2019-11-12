dd0acae5e0e1f3f0f56d62496b8fb5a534e1d24b
Mustapha Habeeb <43966404+Habeeb142@users.noreply.github.com>
2019-11-12 09:27:49 +0100

    Create server.js

---
+++ server.js
@@ -0,0 +1,32 @@
+//making express available::::::::::::::::::::::::::::::::::::::::::::::::
+var express = require('express');
+const server = express();
+var request = require('request');
+var path = require("path");
+//connecting to locahost::::::::::::::::::::::::::::::::::::::::::::::::::
+const port = server.listen(process.env.PORT || 3000, ()=>{
+    console.log("app is listening to port sir!");
+});
+
+//setting engine to ejs::::::::::::::::::::::::::::::::::::::::::::::::::
+server.set('view engine', 'ejs');
+
+
+//middlewares:::
+server.use(express.static(__dirname+'/public'));
+
+//requiring body-parser::::::::::::::::::::::::::::::::::::::::::::::::::::
+var bodyParser = require('body-parser');
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({extended:true}));
+
+
+
+
+// Serve static files....
+server.use(express.static(__dirname + '/dist/biolaahmed')
+
+// Send all requests to index.html for mustaphahabeeb.herokuapp.com::::::::::::
+server.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/biolaahmed/index.html'));
+});
